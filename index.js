bf0494d686475d8565070fce0b3d2d2f784a3526
Kurounin <wiseon3@yahoo.com>
2016-07-22 10:07:57 +0300

    Fixed indentation. Published changes to npm.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.registerRoute = function(hook_name, args, cb) {
-		args.app.get("/auth_session", function(req, res) {
+	args.app.get("/auth_session", function(req, res) {
 		var r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
 
 		r += '<html>' + "\n";
@@ -15,9 +15,11 @@ exports.registerRoute = function(hook_name, args, cb) {
 
 		if (req.query.padName) {
 			var redirectUrl = '/p/';
+
 			if (req.query.groupID) {
 				redirectUrl += req.query.groupID + '$';
 			}
+
 			redirectUrl += req.query.padName
 			r += 'document.location.href="' + redirectUrl + '";' + "\n";
 		}
@@ -26,6 +28,6 @@ exports.registerRoute = function(hook_name, args, cb) {
 		r += '</body>' + "\n";
 		r += '</html>' + "\n";
 
-				res.send(r);
-		});
+		res.send(r);
+	});
 };
