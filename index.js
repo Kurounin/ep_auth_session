230968766666fe37f94ce46006e82c762c5e4dbf
Kurounin <wiseon3@yahoo.com>
2021-06-09 15:40:10 +0300

    refactor: keep any extra query string params when redirecting

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-var URL = require('url').URL;
-var settings = require('ep_etherpad-lite/node/utils/Settings');
+const stringify = require('querystring').stringify;
 
 exports.registerRoute = (hookName, args) => {
-	args.app.get("/auth_session", function(req, res) {
-		var r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
+  args.app.get("/auth_session", function(req, res) {
+    let r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
 
-		r += '<html>' + "\n";
-		r += '<head>' + "\n";
-		r += '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">' + "\n";
-		r += '</head>' + "\n";
-		r += '<body>' + "\n";
-		r += '<script type="text/javascript">' + "\n";
+    r += '<html>' + "\n";
+    r += '<head>' + "\n";
+    r += '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">' + "\n";
+    r += '</head>' + "\n";
+    r += '<body>' + "\n";
+    r += '<script type="text/javascript">' + "\n";
 
-		if (req.query.sessionID) {
-			r += 'document.cookie = "sessionID=' + encodeURIComponent(req.query.sessionID) + '; path=/; SameSite=None; Secure;";' + "\n";
-		}
+    if (req.query.sessionID) {
+      r += 'document.cookie = "sessionID=' + encodeURIComponent(req.query.sessionID) + '; path=/; SameSite=None; Secure;";' + "\n";
+    }
 
-		if (req.query.padName) {
-			var redirectUrl = '/p/';
+    if (req.query.padName) {
+      let redirectUrl = '/p/';
 
-			if (req.query.groupID) {
-				redirectUrl += encodeURIComponent(req.query.groupID) + '$';
-			}
+      if (req.query.groupID) {
+        redirectUrl += encodeURIComponent(req.query.groupID) + '$';
+      }
 
-			redirectUrl += encodeURIComponent(req.query.padName);
+      redirectUrl += encodeURIComponent(req.query.padName);
 
-			if (req.query.lang) {
-				redirectUrl += "?lang=" + encodeURIComponent(req.query.lang);
-			}
+      const query = {};
 
-            var urlHelper = new URL(
-                redirectUrl,
-                `${req.protocol}://${req.get("Host") || "example.org"}`
-            );
-            Object.keys(settings.padOptions).forEach(function(option) {
-                if (option in req.query) {
-                    urlHelper.searchParams.set(option, req.query[option]);
-                }
-            });
-            redirectUrl += urlHelper.search;
+      for (const queryKey in req.query) {
+        if (!req.query.hasOwnProperty(queryKey) || queryKey === "sessionID" || queryKey === "groupID" || queryKey === "padName") {
+          continue;
+        }
+        
+        query[queryKey] = req.query[queryKey];
+      }
 
-			r += 'document.location.href="' + redirectUrl + '";' + "\n";
-		}
+      if (Object.keys(query).length > 0) {
+        redirectUrl += "?" + stringify(query);
+      }
 
-		r += '</script>' + "\n";
-		r += '</body>' + "\n";
-		r += '</html>' + "\n";
+      r += 'document.location.href="' + redirectUrl + '";' + "\n";
+    }
 
-		res.send(r);
-	});
+    r += '</script>' + "\n";
+    r += '</body>' + "\n";
+    r += '</html>' + "\n";
+
+    res.send(r);
+  });
 };
